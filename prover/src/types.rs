79d68332930c79f07536368e6544a30d3f86b2f4
Sergei Shulepov <s.pepyakin@gmail.com>
2025-06-16 17:19:31 +0200

    chore: update binius revision

--- prover/src/types.rs
+++ prover/src/types.rs
@@ -5,7 +5,17 @@
 
 use binius_field::arch::OptimalUnderlier;
 use binius_field::as_packed_field::PackedType;
-use binius_m3::builder::B128;
+use binius_m3::builder::{Boundary, B128};
 
 /// The preferred packed field type used by the prover
 pub type ProverPackedField = PackedType<OptimalUnderlier, B128>;
+
+/// Statement describing the circuit instance for proving and verification.
+///
+/// This mirrors the struct that used to be provided by `binius_m3`.
+/// It simply bundles the channel boundaries together with the table sizes.
+#[derive(Debug, Clone)]
+pub struct Statement {
+    pub boundaries: Vec<Boundary<B128>>,
+    pub table_sizes: Vec<usize>,
+}
